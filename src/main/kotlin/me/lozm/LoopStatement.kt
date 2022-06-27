51d318f113169c81d0932914f5f5bcee1ef3b4c5
junlee <laonzenamoon@gmail.com>
2022-06-27 21:37:27 +0900

    feat: 반복 제어문 관련 기능 연습 추가

---
+++ src/main/kotlin/me/lozm/LoopStatement.kt
@@ -0,0 +1,34 @@
+package me.lozm
+
+fun main(args: Array<String>) {
+    val items = listOf("apple", "banana", "avocado", "kiwifruit")
+    printItemUsingForLoop(items)
+    printItemUsingWhileLoop(items)
+    streamApi(items)
+}
+
+private fun streamApi(items: List<String>) {
+    items.filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.uppercase() }
+        .forEach { println(it) }
+}
+
+private fun printItemUsingWhileLoop(items: List<String>) {
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
+}
+
+private fun printItemUsingForLoop(items: List<String>): List<String> {
+    for (item in items) {
+        println(item)
+    }
+
+    for (index in items.indices) {
+        println("item at $index is ${items[index]}")
+    }
+    return items
+}
