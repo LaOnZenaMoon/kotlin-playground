9d69ceb825e9706b6f8a3a24d5082a7aa6b93414
junlee <laonzenamoon@gmail.com>
2022-07-06 16:53:18 +0900

    feat: 조건 제어문 관련 기능 연습 추가

--- src/main/kotlin/me/lozm/ConditionalStatement.kt
+++ src/main/kotlin/me/lozm/ConditionalStatement.kt
@@ -8,6 +8,34 @@ fun main(args: Array<String>) {
     printWhen(items)
     val stringLength = getStringLength("apple")
     println("stringLength = $stringLength")
+
+    val max = max(1, 2)
+    println(max)
+
+    val gender = convertGender("M")
+    println(gender)
+
+    val hasPrefix = hasPrefix("")
+    println(hasPrefix)
+}
+
+fun hasPrefix(s: Any) = when(s) {
+    // java instanceOf -> kotlin is
+    // is -> auto type casting(Any -> String, Int...)
+    is String -> s.startsWith("prefix")
+    is Int -> s.inc()
+    else -> false
+}
+
+fun convertGender(gender: String): String {
+    return when(gender) {
+        "M", "F" -> "Male or Female"
+        else -> "Unknown"
+    }
+}
+
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a else b
 }
 
 private fun getStringLength(obj: Any): Int? {
@@ -17,6 +45,7 @@ private fun getStringLength(obj: Any): Int? {
 }
 
 private fun printWhen(items: List<String>) {
+    // java switch -> kotlin when
     when {
         "orange" in items -> println("juicy")
         "apple" in items -> println("apple is fine too")
